2bc106499172629a15e0f50c1c51459bb1a83036
nhmood <nhmood@goosecode.com>
2013-08-26 12:20:08 -0400

    Added controls for shapdar parameters
    Added control section to select line width, plot speed,
    plot type (distance vs x/y) and line color

--- js/shapdar.js
+++ js/shapdar.js
@@ -20,3 +20,38 @@ var plot = new Plot(shape, "#shapnal1", 400, 500);
 var animation = new Animation(shape, plot);
 
 
+
+// JQuery stuff for option changing
+// Line width change
+$('#shapnal1 input.lw').change(function(e){
+	$('#shapnal1 span.lw').text(this.value);
+	shape.ctx.lineWidth = this.value;
+	plot.ctx.lineWidth = this.value;
+});
+
+
+// Speed change
+$('#shapnal1 input.ps').change(function(e){
+	$("#shapnal1 span.ps").text(this.value);
+	animation.nPlot = parseInt(this.value);
+})
+
+// Animation change
+$('#shapnal1 select.anim').change(function(e){
+	animation.animationFunction = plot[this.value].bind(animation.plot);
+});
+
+
+
+// Line color change
+$('#shapnal1 select.lc').change(function(e){
+	if (this.value == "red"){
+		shape.ccol = "blue";
+	}
+	else {
+		shape.ccol = "red";
+	}
+	shape.ctx.strokeStyle = this.value;
+	plot.ctx.strokeStyle = this.value
+})
+
